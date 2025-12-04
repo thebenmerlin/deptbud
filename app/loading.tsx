04205e142c7f038786746ba57a96fcef2459d2c1
thebenmerlin <benmerlin1969@gmail.com>
2025-12-04 20:08:39 +0530

    chore: wipe repo (remove all tracked files)

--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="p-8 flex items-center justify-center min-h-screen">
-      <div className="animate-spin">
-        <div className="w-12 h-12 border-4 border-gray-200 border-t-blue-600 rounded-full"></div>
-      </div>
-    </div>
-  );
-}
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-// app/page.tsx
-
-import { redirect } from "next/navigation";
-
-export default function HomePage() {
-  redirect("/dashboard");
-}
